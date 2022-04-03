2478d13f699b03793af0cb34c097e469b86da6e5
Ed Torres <edalscript@gmail.com>
2022-04-03 15:31:21 +0300

    sign in and sign up with Google functionalities

--- src/routes/sign-in/sign-in.jsx
+++ src/routes/sign-in/sign-in.jsx
@@ -1,13 +1,20 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import SignUp from "../../components/sign-up/sign-up";
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+}
+    from "../../utils/firebase/firebase.utils";
+
 
 const SignIn = () => {
     const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
+        const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     }
     return (
         <div>
             <h1>Sign In Page Test</h1>
+            <SignUp />
             <button onClick={logGoogleUser}>Sign in with Google</button>
         </div>
     )
