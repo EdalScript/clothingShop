ac35f4dd0d9dbdf2354e11f0f6b1c432988602f2
Ed Torres <edalscript@gmail.com>
2022-06-26 17:56:57 -0500

    price tag fix

--- src/components/checkout-item/checkout-item.jsx
+++ src/components/checkout-item/checkout-item.jsx
@@ -21,7 +21,7 @@ const CheckoutItem = ({ cartItem }) => {
                 <span className='value'>{quantity}</span>
                 <div className='arrow' onClick={addItemHandler}>&#10095;</div>
             </span>
-            <span className='price'>{price}</span>
+            <span className='price'>€{price}.00</span>
             <div className='remove-button' onClick={clearItemHandler}>&#10005;</div>
         </div>
     )
